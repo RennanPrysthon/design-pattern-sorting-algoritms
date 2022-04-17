5daac025d493df99c2da43f24d952d43461f011a
Rennan Prysthon <rennan.prysthon@liferay.com>
2022-04-17 13:58:27 -0300

    rename project

---
+++ src/br/com/ifpe/sorting/SortingAlgorithms.java
@@ -0,0 +1,10 @@
+package br.com.ifpe.sorting;
+
+import javax.swing.*;
+
+public class SortingAlgorithms extends JFrame {
+
+    public SortingAlgorithms() {
+        super("Sorting Algorithms");
+    }
+}
--- src/br/com/ifpe/sorting/SortingAlgoritms.java
+++
@@ -1,4 +0,0 @@
-package br.com.ifpe.sorting;
-
-public class SortingAlgoritms {
-}
